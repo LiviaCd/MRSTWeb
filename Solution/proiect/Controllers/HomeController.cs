c52b2d670ae806241e92b4fa6015bcfed7cf14b5
LiviaCd <120571940+LiviaCd@users.noreply.github.com>
2024-02-28 12:29:59 +0200

    Footer modified

--- Solution/proiect/Controllers/HomeController.cs
+++ Solution/proiect/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Antlr.Runtime.Tree;
+using proiect.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,44 +8,70 @@ using System.Web.Mvc;
 
 namespace proiect.Controllers
 {
-    public class HomeController : Controller
-    {
-        // GET: Home
-        public ActionResult Index()
-        {
-            return View();
-        }
+     public class HomeController : Controller
+     {
+          // GET: Home
+          public ActionResult Index()
+          {
+               UserData u = new UserData();
+               u.UserName = "Customer";
 
-        public ActionResult UserPage()
-        {
-            return View();
-        }
+               return View(u);
+          }
 
-        public ActionResult LogIn()
-        {
-            return View();
-        }
+          public ActionResult UserPage()
+          {
+               return View();
+          }
 
-        public ActionResult SignIn()
-        {
-            return View();
-        }
+          public ActionResult LogIn()
+          {
+               return View();
+          }
+
+          public ActionResult SignIn()
+          {
+               return View();
+          }
 
           public ActionResult About()
           {
-               return View();
+               UserData u = new UserData();
+               u.UserName = "Customer";
+               u.BloodType = new List<string> { "Grupa 0", "Grupa A", "Grupa B", "Grupa AB" };
+               return View(u);
           }
           public ActionResult News()
           {
-               return View();
+               UserData u = new UserData();
+               u.UserName = "Customer";
+               return View(u);
           }
           public ActionResult Contact()
           {
-               return View();
+               UserData u = new UserData();
+               u.UserName = "Customer";
+               u.BloodType = new List<string> { "Grupa 0", "Grupa A", "Grupa B", "Grupa AB" };
+               return View(u);
           }
           public ActionResult Pretest()
           {
                return View();
+          }
+          public ActionResult BloodType()
+          {
+               var type = Request.QueryString["t"];
+
+               UserData u = new UserData();
+               u.UserName = "Customer";
+               u.SingleType = type;
+
+               return View(u);
+          }
+          [HttpPost]
+          public ActionResult BloodType(string btn) 
+          {
+               return RedirectToAction("BloodType", "Home", new {@t = btn}); 
           }
 
 
