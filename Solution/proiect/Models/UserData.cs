9dc8510833292eec32f07878202417cd4234082f
LiviaCd <120571940+LiviaCd@users.noreply.github.com>
2024-03-03 14:07:46 +0200

    UserData-update

---
+++ Solution/proiect/Models/UserData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace proiect.Models
+{
+     public class BloodTypeModel
+     {
+          public string Name { get; set; }
+          public string ImageUrl { get; set; } 
+     }
+     public class UserData
+     {
+          public string UserName { get; set; }
+          public string SingleType { get; set; }
+          public List<BloodTypeModel> BloodType { get; set; }
+     }
+}
